1196cb2f547b89b276b3888308ad36db2e732675
Максим Зосимов <mizo-y@yandex.ru>
2023-01-25 18:32:49 +0000

    все 10 задач выполнены.

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+flag = True
+while flag:
+    adress = input('\nВведите IP: ').split('.')
+    if len(adress) != 4:
+         print('Адрес — это четыре числа, разделённые точками.')
+    else:
+        for i in range(len(adress)):
+            part = adress[i]
+            if not part.isdigit():
+                print(f'{part} — число указано не верно.')
+                break
+            if int(part) > 255:
+                print(f'{part} — число указано не верно.')
+                break
+        else:
+            flag = False
+print('IP-адрес корректен.')
--- Module18/08_ticker/main.py
+++ Module18/08_ticker/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+def shift_compare(list_1, list_2):
+    for i in range(len(list_2)):
+        tail = list_2[len(list_2)-1]
+        list_2.pop(len(list_2)-1)
+        list_2.insert(0, tail)
+        if list_1 == list_2:
+          return i+1
+
+
+first = list(input('Первая строка: '))  # qwerty
+second = list(input('Вторая строка: '))  # rtyqwe
+result = shift_compare(first, second)
+
+if result is None:
+    print('\nПервую строку нельзя получить из второй с помощью циклического сдвига.')
+else:
+    print(f'\nПервая строка получается из второй со сдвигом {result}.')
