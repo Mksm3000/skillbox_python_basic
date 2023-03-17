e95be682698bec79331581649e2c9b072c2da69d
Maksim Zosimov <mizo-y@yandex.ru>
2023-03-17 13:36:27 +0100

    требуется помощь

--- Module21/03_deep_copy/main.py
+++ Module21/03_deep_copy/main.py
@@ -1,14 +1,44 @@
+import copy
+
+
+def func(dikt, word, change):
+    for key, value in dikt.items():
+        if isinstance(value, str):
+            if word in value:
+                dikt[key] = value.replace(word, change)
+        elif isinstance(value, dict):
+            sub_dikt = value
+            for sub_key, sub_value in sub_dikt.items():
+                func(sub_value, word, change)
+    return dikt
+
+
 site = {
     'html': {
         'head': {
             'title': 'Куплю/продам телефон недорого'
         },
         'body': {
-            'h2': 'У нас самая низкая цена на iphone',
+            'h2': 'У нас самая низкая цена на iPhone',
             'div': 'Купить',
-            'p': 'продать'
+            'p': 'Продать'
         }
     }
 }
 
-# TODO здесь писать код
+time = int(input('\nСколько сайтов: '))
+
+while time:
+    search = input('\nКакое слово ищем для замены? ')
+    brand = input('Введите название продукта для нового сайта: ')
+    result = func(copy.copy(site), search, brand)
+
+    for key, value in result.items():
+        if isinstance(value, dict):
+            print(key)
+            for sub_key, sub_value in value.items():
+                print(sub_key, sub_value)
+        else:
+            print(key, value)
+
+    time -= 1
