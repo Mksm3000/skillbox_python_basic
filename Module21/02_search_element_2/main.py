3a1a3a1853c0f6998557cb1de5f8e5daa089d91d
Maksim Zosimov <mizo-y@yandex.ru>
2023-03-21 08:16:45 +0100

    все задачи решены

--- Module21/02_search_element_2/main.py
+++ Module21/02_search_element_2/main.py
@@ -1,1 +1,54 @@
-# TODO здесь писать код
+def find(struct, key, depth):
+    if key in struct:
+        return struct[key]
+
+    while depth >= 1:
+        for sub_struct in struct.values():
+            if isinstance(sub_struct, dict):
+                result = find(sub_struct, key, depth)
+                if result:
+                    break
+        else:
+            result = None
+        depth -= 1
+        return result
+
+
+site = {
+    'html': {
+        'head': {
+            'title': 'Мой сайт'
+        },
+        'body': {
+            'h2': 'Здесь будет мой заголовок',
+            'div': 'Тут, наверное, какой-то блок',
+            'p': 'А вот здесь новый абзац'
+        }
+    }
+}
+
+user_key = input('\nИскомый ключ: ')
+user_depth = input('Хотите ввести максимальную глубину? Y/N: ').lower()
+max_depth = 100
+if user_depth == 'n':
+    max_depth = max_depth
+elif user_depth == 'y':
+    max_depth = int(input('Введите максимальную глубину: '))
+
+value = find(site, user_key, max_depth)
+
+if value:
+    print(f'Значение ключа: {value}')
+else:
+    print('Упс! Такого ключа в структуре сайта нет.')
+
+# Пример 1:
+# Введите искомый ключ: head
+# Хотите ввести максимальную глубину? Y/N: n
+# Значение ключа: {'title': 'Мой сайт'}
+#
+# Пример 2:
+# Введите искомый ключ: head
+# Хотите ввести максимальную глубину? Y/N: y
+# Введите максимальную глубину: 1
+# Значение ключа: None
--- Module21/03_deep_copy/main.py
+++ Module21/03_deep_copy/main.py
@@ -1,15 +1,16 @@
 import copy
 
 
-def func(dikt, word, change):
+def func(dikt, change, z_args):
     for key, value in dikt.items():
         if isinstance(value, str):
-            if word in value:
-                dikt[key] = value.replace(word, change)
+            for z_arg in z_args:
+                if z_arg in value:
+                    dikt[key] = value.replace(z_arg, change)
         elif isinstance(value, dict):
             sub_dikt = value
-            for sub_key, sub_value in sub_dikt.items():
-                func(sub_value, word, change)
+            for subb_key, subb_value in sub_dikt.items():
+                func(subb_value, change, z_args)
     return dikt
 
 
@@ -27,18 +28,20 @@ site = {
 }
 
 time = int(input('\nСколько сайтов: '))
+result_total = dict()
+zamena = ('телефон',)
 
 while time:
-    search = input('\nКакое слово ищем для замены? ')
-    brand = input('Введите название продукта для нового сайта: ')
-    result = func(copy.copy(site), search, brand)
+    result = copy.deepcopy(site)
+    brand = input('\nВведите название продукта для нового сайта: ')
+    result_total[str(brand)] = func(result, brand, zamena)
 
-    for key, value in result.items():
-        if isinstance(value, dict):
-            print(key)
-            for sub_key, sub_value in value.items():
-                print(sub_key, sub_value)
-        else:
-            print(key, value)
+    for name in result_total:
+        print(name, result_total[name])
+    print()
+    zamena = list(zamena)
+    zamena.append(brand)
+    zamena = tuple(zamena)
+#    print('zamena:', zamena)
 
     time -= 1
