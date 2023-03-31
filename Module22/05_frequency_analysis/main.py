12efbf9765ec4faf9dbbdadf798d12f52fe1ee1c
Maksim Zosimov <mizo-y@yandex.ru>
2023-03-31 17:47:03 +0200

    модуль 22 задачи 1-6 выполнены

--- Module22/05_frequency_analysis/main.py
+++ Module22/05_frequency_analysis/main.py
@@ -1,1 +1,39 @@
-# TODO здесь писать код
+file = open('text.txt', 'r', encoding='utf-8')
+
+letter_dict = dict()
+for letter in file.read():
+    if letter.isalpha():
+        letter = letter.lower()
+        if letter in letter_dict:
+            letter_dict[letter] += 1
+        else:
+            letter_dict[letter] = 1
+
+file.close()
+
+#print(letter_dict)
+letter_summ = sum(letter_dict.values())
+
+for key, value in letter_dict.items():
+    letter_dict[key] = round(value/letter_summ, 3)
+#print(letter_dict)
+
+
+count_dict = dict()
+for key, value in letter_dict.items():
+    if value in count_dict:
+        pop_data = count_dict.pop(value)
+        merge_data = key + pop_data
+        count_dict[value] = ''.join(sorted(merge_data))
+    else:
+        count_dict[value] = key
+
+#print(count_dict)
+
+file = open('analysis.txt', 'w', encoding='utf-8')
+
+for key, value in count_dict.items():
+    for element in value:
+        file.write(str(element) + ' ' + str(key) + '\n')
+
+file.close()
--- Module22/06_war_and_peace/main.py
+++ Module22/06_war_and_peace/main.py
@@ -1,1 +1,40 @@
-# TODO здесь писать код
+from zipfile import ZipFile
+
+file_name = str()
+with ZipFile('voyna-i-mir.zip', 'r') as myzip:
+    for item in myzip.namelist():
+        file_name = item
+
+with ZipFile('voyna-i-mir.zip', 'r') as myzip:
+    myzip.extractall()
+
+myzip.close()
+
+file = open(file_name, 'r', encoding='utf-8')
+
+letter_dict = dict()
+for letter in file.read():
+    if letter.isalpha():
+        if letter in letter_dict:
+            letter_dict[letter] += 1
+        else:
+            letter_dict[letter] = 1
+
+file.close()
+
+letter_list = []
+for key, value in letter_dict.items():
+    letter_list.append([key, value])
+
+def right(data):
+    return data[1]
+
+
+letter_list.sort(key=right,reverse=True)
+
+result = open('result.txt', 'w', encoding='utf-8')
+
+for element in letter_list:
+    result.write(str(element[0]) + ': ' + str(element[1]) + '\n')
+
+result.close()
