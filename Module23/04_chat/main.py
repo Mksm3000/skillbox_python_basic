2179307f351a9e596052980ad60d805d34cfd693
Maksim Zosimov <mizo-y@yandex.ru>
2023-04-07 11:05:38 +0200

    модуль 23 все задачи выполнены

--- Module23/04_chat/main.py
+++ Module23/04_chat/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+
+while True:
+    name = input('\nВведите ваше имя: ')
+    print('1. Посмотреть текущий текст чата.\n2. Отправить сообщение.\n')
+    choice = int(input('Введите номер действия: '))
+    if choice == 1:
+        print('\nТекущий текст чата:')
+        try:
+            with open('chat.txt', 'r', encoding='utf-8') as file_chat:
+                for line in file_chat:
+                    line = line.rstrip('\n')
+                    print(line)
+        except FileNotFoundError:
+            print('Чат пока пустой. Вы - первыйнах!')
+    elif choice == 2:
+        message = input('Введите текст сообщения: ')
+        with open('chat.txt', 'a', encoding='utf-8') as file_input:
+            file_input.write(name + ': ' + message + '\n')
