fd11a389aec2e624bcf9a37c95634cc5939fa95e
Maksim Zosimov <mizo-y@yandex.ru>
2023-05-16 15:18:16 +0200

    задача 2. исправлено

--- Module26/02_files_path/main.py
+++ Module26/02_files_path/main.py
@@ -3,20 +3,34 @@
 # генерирует пути всех встреченных файлов.
 
 import os
+import time
 from collections.abc import Iterable
 
 
 def gen_files_path(my_path: str, target: str) -> Iterable[str]:
-    print(f'\nСписок файлов в директории "{target}":\n')
-    for root, dirs, files in os.walk(my_path):
-        for direct in dirs:
-            for file in files:
-                if target in root:
-                    yield f'{root}\\{direct}\\{file}'
+    for directory, folders, files in os.walk(my_path):
+        if target == directory:
+            break
+        for file in files:
+            yield os.path.join(directory, file)
 
 
 root_path = os.path.abspath(os.path.join(os.path.sep))
-dir_name = 'Python_Basic\Module'
 
-for element in gen_files_path(root_path, dir_name):
-    print(element)
+dir_name = 'PycharmProjects'
+print(f'\nИщем директорию "{dir_name}".')
+
+for directory, folders, files in os.walk(root_path):
+    if dir_name in folders:
+        print('Да, такая директория существует', end='')
+
+        dir_path = os.path.join(directory,dir_name)
+        print(f'"{dir_path}"\n')
+
+        # чтобы успеть увидеть комментарий с адресом
+        time.sleep(2)
+
+        data = gen_files_path(root_path, dir_path)
+
+        for element in data:
+            print(element)
