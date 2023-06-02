c6119bb35b336a63a3fb1a6d9b5e9a15135c4d7c
Maksim Zosimov <mizo-y@yandex.ru>
2023-06-02 12:11:42 +0200

    задачи выполнены

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+
+class File:
+
+    def __init__(self, file_name: str) -> None:
+        self.file_name = file_name
+        self.file = None
+
+    def __enter__(self) -> object:
+        try:
+            self.file = open(self.file_name, 'r', encoding='utf8')
+        except FileNotFoundError:
+            self.file = open(self.file_name, 'x', encoding='utf8')
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+        if self.file:
+            self.file.close()
+        return True
+
+
+with File("test_reading.txt") as file:
+    file.write("Всем привет!")
+    print(file.read())
+
+with File("test_writing.txt") as file:
+    file.write("Всем привет!")
+    print(file.read())
+
--- Module28/03_date/main.py
+++ Module28/03_date/main.py
@@ -1,1 +1,48 @@
-# TODO здесь писать код
+from typing import Any
+from datetime import date
+
+
+class Date:
+
+    def __init__(self, day: int, month: int, year: int) -> None:
+        self.__date = date(year, month, day)
+
+    @classmethod
+    def fragmentation(cls, data_string: str) -> tuple:
+        return tuple(map(int, data_string.split('-')))
+
+    @classmethod
+    def is_date_valid(cls, data_string: str) -> bool:
+        try:
+            day, month, year = cls.fragmentation(data_string)
+            if date(year, month, day):
+                return True
+        except:
+            return False
+
+    @classmethod
+    def from_string(cls, data_string: str) -> Any:
+        if cls.is_date_valid(data_string):
+            day, month, year = cls.fragmentation(data_string)
+            result = cls(day, month, year)
+            return result
+        else:
+            return f'Ошибка в данных'
+
+    def __str__(self) -> str:
+        return self.__date.strftime('День: %d (%A)\tМесяц: %m (%B)\tГод: %Y')
+
+
+test_date = Date.from_string('10-12-2077')
+print(test_date)
+
+test_date = Date.from_string('12-04-1961')
+print(test_date)
+
+print(Date.is_date_valid('10-12-2077'))
+print(Date.is_date_valid('40-12-2077'))
+
+# Результат:
+# День: 10	Месяц: 12	Год: 2077
+# True
+# False
