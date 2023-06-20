e8ad2ecde253dd3bf6cba4e092c7349558a02c9b
Maksim Zosimov <mizo-y@yandex.ru>
2023-06-20 14:54:36 +0200

    задания 1-3 готовы

--- Module30/03_palindrome_the_return/main.py
+++ Module30/03_palindrome_the_return/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def can_be_poly(text: str) -> bool:
+    return text[::] == text[::-1]
+
+
+print(can_be_poly('abcba'))
+print(can_be_poly('abbbc'))
+
+# Результат:
+# ```
+# True
+# False
